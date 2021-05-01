65ed1ff5a3fc37195af1ea341116608d8a0ceb83
Juncheng Tang <juncheng.tang@yale.edu>
2021-05-01 19:55:36 +0000

    edsr and cae model train/test

---
+++ src/models/motion_blur3.py
@@ -0,0 +1,34 @@
+from registry import registry
+from models.model_base import Model, StandardTransform, StandardNormalization
+import torchvision.models as torch_models
+import torch
+from collections import OrderedDict 
+def classifier_loader():
+    model = torch_models.resnet18()
+    
+    checkpoint = torch.load('/home/jtang/Desktop/motion_blur_model/checkpoint.pth3.tar')
+
+    
+    prefix = 'module.'
+    n_clip = len(prefix)
+    adapted_dict = {k[n_clip:]: v for k, v in checkpoint['state_dict'].items()
+                    if k.startswith(prefix)}
+
+    
+    model.load_state_dict(adapted_dict)
+    
+    # model.load_state_dict(new_state_dict)
+    # optimizer.load_state_dict(checkpoint['optimizer'])
+    return model
+
+registry.add_model(
+    Model(
+        name = 'motion_blur3',
+        transform = StandardTransform(img_resize_size=256, img_crop_size=224),
+        normalization = StandardNormalization(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        classifier_loader = classifier_loader,
+        eval_batch_size = 128,
+        # OPTIONAL
+        arch = 'resnet18',
+    )
+)
