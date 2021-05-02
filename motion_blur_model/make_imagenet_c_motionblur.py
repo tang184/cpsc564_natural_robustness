b018a79681a541f7ff03557b8316acd58758f296
Yifan Li <34142106+lyffreya@users.noreply.github.com>
2021-05-02 23:39:47 +0000

    update motion blur augmentation

---
+++ motion_blur_model/make_imagenet_c_motionblur.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+#
+import os
+from PIL import Image
+import os.path
+import time
+import torch
+import torchvision.datasets as dset
+import torchvision.transforms as trn
+import torch.utils.data as data
+import numpy as np
+
+#from PIL import Image
+
+# /////////////// Data Loader ///////////////
+
+
+IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
+
+
+def is_image_file(filename):
+    """Checks if a file is an image.
+    Args:
+        filename (string): path to a file
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    filename_lower = filename.lower()
+    return any(filename_lower.endswith(ext) for ext in IMG_EXTENSIONS)
+
+
+def find_classes(dir):
+    classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    return classes, class_to_idx
+
+
+def make_dataset(dir, class_to_idx):
+    images = []
+    dir = os.path.expanduser(dir)
+    for target in sorted(os.listdir(dir)):
+        d = os.path.join(dir, target)
+        if not os.path.isdir(d):
+            continue
+
+        for root, _, fnames in sorted(os.walk(d)):
+            for fname in sorted(fnames):
+                if is_image_file(fname):
+                    path = os.path.join(root, fname)
+                    item = (path, class_to_idx[target])
+                    images.append(item)
+
+    return images
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+
+class DistortImageFolder(data.Dataset):
+    def __init__(self, root, method, severity, transform=None, target_transform=None,
+                 loader=default_loader):
+        classes, class_to_idx = find_classes(root)
+        imgs = make_dataset(root, class_to_idx)
+        if len(imgs) == 0:
+            raise (RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                                                                             "Supported image extensions are: " + ",".join(
+                IMG_EXTENSIONS)))
+
+        self.root = root
+        self.method = method
+        self.severity = severity
+        self.imgs = imgs
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.idx_to_class = {v: k for k, v in class_to_idx.items()}
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        path, target = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+            img = self.method(img, self.severity)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        #save_path = '/share/data/vision-greg/DistortedImageNet/JPEG/' + self.method.__name__ + \
+        #            '/' + str(self.severity) + '/' + self.idx_to_class[target]
+        # /home/jtang/Desktop/DistortedImageNet/JPEG/
+        save_path = '/data/imagenet_augment_MotionBlur/' + self.method.__name__ + \
+                    '/' + str(self.severity) + '/' + self.idx_to_class[target]
+        
+        #root="/share/data/vision-greg/ImageNet/clsloc/images/val",
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        save_path += path[path.rindex('/'):]
+
+        Image.fromarray(np.uint8(img)).save(save_path, quality=85, optimize=True)
+
+        return 0  # we do not care about returning the data
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+# /////////////// Distortion Helpers ///////////////
+
+import skimage as sk
+from skimage.filters import gaussian
+from io import BytesIO
+from wand.image import Image as WandImage
+from wand.api import library as wandlibrary
+import wand.color as WandColor
+import ctypes
+from PIL import Image as PILImage
+import cv2
+from scipy.ndimage import zoom as scizoom
+from scipy.ndimage.interpolation import map_coordinates
+import warnings
+
+warnings.simplefilter("ignore", UserWarning)
+
+
+def auc(errs):  # area under the alteration error curve
+    area = 0
+    for i in range(1, len(errs)):
+        area += (errs[i] + errs[i - 1]) / 2
+    area /= len(errs) - 1
+    return area
+
+
+def disk(radius, alias_blur=0.1, dtype=np.float32):
+    if radius <= 8:
+        L = np.arange(-8, 8 + 1)
+        ksize = (3, 3)
+    else:
+        L = np.arange(-radius, radius + 1)
+        ksize = (5, 5)
+    X, Y = np.meshgrid(L, L)
+    aliased_disk = np.array((X ** 2 + Y ** 2) <= radius ** 2, dtype=dtype)
+    aliased_disk /= np.sum(aliased_disk)
+
+    # supersample disk to antialias
+    return cv2.cv2.GaussianBlur(aliased_disk, ksize=ksize, sigmaX=alias_blur)
+
+
+# Tell Python about the C method
+wandlibrary.MagickMotionBlurImage.argtypes = (ctypes.c_void_p,  # wand
+                                              ctypes.c_double,  # radius
+                                              ctypes.c_double,  # sigma
+                                              ctypes.c_double)  # angle
+
+
+# Extend wand.image.Image class to include method signature
+class MotionImage(WandImage):
+    def motion_blur(self, radius=0.0, sigma=0.0, angle=0.0):
+        wandlibrary.MagickMotionBlurImage(self.wand, radius, sigma, angle)
+
+
+# modification of https://github.com/FLHerne/mapgen/blob/master/diamondsquare.py
+def plasma_fractal(mapsize=256, wibbledecay=3):
+    """
+    Generate a heightmap using diamond-square algorithm.
+    Return square 2d array, side length 'mapsize', of floats in range 0-255.
+    'mapsize' must be a power of two.
+    """
+    assert (mapsize & (mapsize - 1) == 0)
+    maparray = np.empty((mapsize, mapsize), dtype=np.float_)
+    maparray[0, 0] = 0
+    stepsize = mapsize
+    wibble = 100
+
+    def wibbledmean(array):
+        return array / 4 + wibble * np.random.uniform(-wibble, wibble, array.shape)
+
+    def fillsquares():
+        """For each square of points stepsize apart,
+           calculate middle value as mean of points + wibble"""
+        cornerref = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
+        squareaccum = cornerref + np.roll(cornerref, shift=-1, axis=0)
+        squareaccum += np.roll(squareaccum, shift=-1, axis=1)
+        maparray[stepsize // 2:mapsize:stepsize,
+        stepsize // 2:mapsize:stepsize] = wibbledmean(squareaccum)
+
+    def filldiamonds():
+        """For each diamond of points stepsize apart,
+           calculate middle value as mean of points + wibble"""
+        mapsize = maparray.shape[0]
+        drgrid = maparray[stepsize // 2:mapsize:stepsize, stepsize // 2:mapsize:stepsize]
+        ulgrid = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
+        ldrsum = drgrid + np.roll(drgrid, 1, axis=0)
+        lulsum = ulgrid + np.roll(ulgrid, -1, axis=1)
+        ltsum = ldrsum + lulsum
+        maparray[0:mapsize:stepsize, stepsize // 2:mapsize:stepsize] = wibbledmean(ltsum)
+        tdrsum = drgrid + np.roll(drgrid, 1, axis=1)
+        tulsum = ulgrid + np.roll(ulgrid, -1, axis=0)
+        ttsum = tdrsum + tulsum
+        maparray[stepsize // 2:mapsize:stepsize, 0:mapsize:stepsize] = wibbledmean(ttsum)
+
+    while stepsize >= 2:
+        fillsquares()
+        filldiamonds()
+        stepsize //= 2
+        wibble /= wibbledecay
+
+    maparray -= maparray.min()
+    return maparray / maparray.max()
+
+
+def clipped_zoom(img, zoom_factor):
+    h = img.shape[0]
+    # ceil crop height(= crop width)
+    ch = int(np.ceil(h / zoom_factor))
+
+    top = (h - ch) // 2
+    img = scizoom(img[top:top + ch, top:top + ch], (zoom_factor, zoom_factor, 1), order=1)
+    # trim off any extra pixels
+    trim_top = (img.shape[0] - h) // 2
+
+    return img[trim_top:trim_top + h, trim_top:trim_top + h]
+
+
+
+
+# /////////////// Distortions ///////////////
+def gaussian_noise(x, severity=1):
+    c = [.08, .12, 0.18, 0.26, 0.38][severity - 1]
+
+    x = np.array(x) / 255.
+    return np.clip(x + np.random.normal(size=x.shape, scale=c), 0, 1) * 255
+
+def motion_blur(x, severity=1):
+    c = [(10, 3), (15, 5), (15, 8), (15, 12), (20, 15)][severity - 1]
+
+    output = BytesIO()
+    x.save(output, format='PNG')
+    x = MotionImage(blob=output.getvalue())
+
+    x.motion_blur(radius=c[0], sigma=c[1], angle=np.random.uniform(-45, 45))
+
+    x = cv2.cv2.imdecode(np.fromstring(x.make_blob(), np.uint8),
+                     cv2.cv2.IMREAD_UNCHANGED)
+
+    if x.shape != (224, 224):
+        return np.clip(x[..., [2, 1, 0]], 0, 255)  # BGR to RGB
+    else:  # greyscale to RGB
+        return np.clip(np.array([x, x, x]).transpose((1, 2, 0)), 0, 255)
+
+
+
+
+
+# /////////////// Further Setup ///////////////
+
+# root="/share/data/vision-greg/ImageNet/clsloc/images/val",
+# root = "/Users/yifanli/Yale/2021FML/AdvRobustness/val"
+# /home/jtang/Desktop/val
+# /data/imagenet/ILSVRC/Data/CLS-LOC/train
+def save_distorted(method=gaussian_noise):
+    for severity in range(1, 6):
+        print(method.__name__, severity)
+        distorted_dataset = DistortImageFolder(
+            root ="/data/imagenet/ILSVRC/Data/CLS-LOC/real_train",
+            method=method, severity=severity,
+            transform=trn.Compose([trn.Resize(256), trn.CenterCrop(224)]))
+        distorted_dataset_loader = torch.utils.data.DataLoader(
+            distorted_dataset, batch_size=100, shuffle=False, num_workers=4)
+
+        for _ in distorted_dataset_loader: continue
+
+
+# /////////////// Display Results ///////////////
+
+print('\nUsing ImageNet data')
+save_distorted(motion_blur)
